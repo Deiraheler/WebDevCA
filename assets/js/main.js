266deeb55cec57a0a110443e98432fa46e84145f
Deiraheler <53556704+Deiraheler@users.noreply.github.com>
2023-04-05 17:35:45 +0100

    index page done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,21 @@
-$(function(){
-    $("#profile-icon").on('click', function(){
+import sharedModule from "../../templates/item";
+
+$(function () {
+    let profile_side_width = $("#profile-side-menu").outerWidth();
+
+    $("#profile-icon").on('click', function () {
         $("#profile-block").toggleClass("open");
+        $("#profile-shadow").width($(window).width() - profile_side_width + "px");
+        $("#profile-shadow").css("margin-left", profile_side_width + "px");
     })
 
-    $(".close-cross").on("click", function(){
+    $(window).resize(function () {
+        $("#profile-shadow").width($(window).width() - profile_side_width + "px");
+    });
+
+
+
+    $(".close-cross").on("click", function () {
         $(this).parent().toggleClass("open");
     })
 
@@ -13,5 +25,11 @@ $(function(){
         speed: 1000,
         dots: true,
         autoplaySpeed: 3000,
-      });
+    });
+
+    //Printing blocks
+    for (let i = 0; i < 6; i++) {
+        let carBlock = sharedModule.template;
+        $('.big-img').append(carBlock);
+    }
 })
