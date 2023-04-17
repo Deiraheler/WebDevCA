fe0fbef504f496e7b177fc6755b315719f1b186d
Deiraheler <53556704+Deiraheler@users.noreply.github.com>
2023-04-17 18:40:49 +0100

    new page created. Fix adaptive problems

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,6 +34,20 @@ $(function () {
         variableWidth: true,
     });
 
+    $('.slider').slick({
+        dots: false,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+        nextArrow: '<button type="button" class="slick-next">Next</button>',
+        asNavFor: '.thumbnails'
+    });
+    $('.thumbnails').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider',
+        focusOnSelect: true
+    });
+
     // Slider adaptive slide image
     var parentBlock = $('.slick-slide');
     var img = $('.slick-slide > img');
@@ -51,12 +65,19 @@ $(function () {
     }
 
     function setImgHeight() {
-        if (img.height() <= parentBlock.height()) {
+        console.log("---------------");
+        console.log("IMG HEIGH: " + img.height());
+        console.log("IMG WIDTH: " + img.width());
+        console.log("BLOCK HEIGH: " + parentBlock.height());
+        console.log("BLOCK WIDTH: " + parentBlock.width());
+        console.log("---------------");
+
+        if (img.height() < parentBlock.height()) {
         img.css({
             'height': '100%',
             'width': 'auto',
         });
-        } else {
+        }else if(img.height() >= parentBlock.height()){
         img.css({
             'height': 'auto',
             'width': '100%',
